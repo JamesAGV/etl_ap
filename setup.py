24f8a3239d15bdd76a9ba0b3326f326a5a522c6d
JamesAGV <jamgarciavi@unal.educ.o>
2023-11-03 21:46:08 -0500

    install requires

--- setup.py
+++ setup.py
@@ -2,10 +2,15 @@ from setuptools import setup
 
 setup(
     name='etl_ap',
-    version='0.1.1',
+    version='0.1.2',
     description='ETL para Andina Pack - Elico Group',
     author='James Garcia',
     author_email='jamgarciavi@unal.edu.co',
     url='',
     packages=['etl_ap'],
+    install_requires=[
+        'pytz',
+        'requests',
+        'snap7',
+    ],
 )
